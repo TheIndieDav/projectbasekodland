0ae00ddec25f3310d450e369895bd949baa0cecd
The indie dav <81372075+TheIndieDav@users.noreply.github.com>
2023-10-06 20:04:55 -0300

    Add files via upload

---
+++ 7bot thing.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hey 7bro, You are {bot.user} Right?')
+
+@bot.command()
+async def buck(ctx, count_buck = 5):
+    await ctx.send("17bucks" * count_buck)
+
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+bot.run("MTE1OTk4NDI3NjU1Nzc5OTQ2NA.GECmfj.jNYk9CdShYHIyUmH7R3O2fNpy2xc_-g7xrdkwU")
