e62faee5c4a8df1f4e1c05ee0e9a887030d8fcc0
The indie dav <81372075+TheIndieDav@users.noreply.github.com>
2023-10-06 20:19:45 -0300

    Update 7bot thing.py

--- 7bot thing.py
+++ 7bot thing.py
@@ -4,15 +4,15 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='7', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print('We have logged in as {bot.user}')
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Hey 7bro, You are {bot.user} Right?')
+    await ctx.send('Hey 7bro, Im {bot.user}, I love quidding all around')
 
 @bot.command()
 async def buck(ctx, count_buck = 5):
